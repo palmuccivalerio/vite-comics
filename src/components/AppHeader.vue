e0d5723c42d4364ee9784f007afe69d04969c90d
palmuccivalerio <palmuccivalerio@gmail.com>
2024-05-03 15:40:09 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,103 @@
+<script>
+export default {
+  data() {
+    return {
+      menu: [
+        {
+          title: "CHARATERS",
+          isActive: false,
+        },
+        {
+          title: "COMICS",
+          isActive: true,
+        },
+        {
+          title: "MOVIES",
+          isActive: false,
+        },
+        {
+          title: "TV",
+          isActive: false,
+        },
+        {
+          title: "GAMES",
+          isActive: false,
+        },
+        {
+          title: "COLLECTIBLES",
+          isActive: true,
+        },
+        {
+          title: "VIDEOS",
+          isActive: false,
+        },
+        {
+          title: "FANS",
+          isActive: false,
+        },
+        {
+          title: "NEWS",
+          isActive: false,
+        },
+        {
+          title: "SHOP",
+          isActive: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<template>
+  <header>
+    <div class="header-logo">
+      <img
+        src="../assets/img/dc-logo.png"
+        alt="Logo  della DC"
+      />
+    </div>
+    <ul class="navigation">
+      <li v-for="item in menu">
+        <a href="" :class="{ active: item.isActive }">{{ item.title }}</a>
+      </li>
+    </ul>
+  </header>
+</template>
+
+<style scoped lang="scss">
+@use "../style/partials/mixin" as *;
+@use "../style/partials/variables" as *;
+
+header {
+
+  .header-logo {
+    text-align: center;
+    display: flex;
+
+  }
+
+  ul {
+    @include flex(row, center, center);
+    list-style-type: none;
+    gap: 1rem;
+    margin-top: 26px;
+
+    a {
+      padding: 8px 10px;
+      text-decoration: none;
+      color: $text-color;
+      dispaly:flex;
+      
+
+      &:hover {
+        color: $primary-dark;
+      }
+
+      &.active {
+        background-color: $primary-color;
+      }
+    }
+  }
+}
+</style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,85 @@
+<script>
+export default {  
+  data() {
+    return {
+      menu: [
+        {
+          title: "DIGITAL COMICS",
+          isActive: false,
+        },
+        {
+          title: "DC MERCHANDISE",
+          isActive: true,
+        },
+        {
+          title: "SUBSCRIPTION",
+          isActive: false,
+        },
+        {
+          title: "COMIC SHOP LOCATOR",
+          isActive: false,
+        },
+        {
+          title: "DC POWER VISA",
+          isActive: false,
+        },
+  
+      ],
+    };
+  },};
+</script>
+
+<template>
+  <section class="products">
+    <h2> -> Content goes here <-- </h2>
+  </section>
+
+ <section>
+  <ul class="digi-navigation">
+      <li v-for="item in menu">
+        <a href="" :class="{ active: item.isActive }">{{ item.title }}</a>
+      </li>
+    </ul>
+ </section>
+
+</template>
+
+<style lang="scss" scoped>
+@use "../style/partials/mixin" as *;
+
+h2{
+  text-align: left;
+}
+
+
+section {
+  background-color: black;
+  
+}
+.products {
+  margin-top: 0rem;
+  padding: 3rem 0;
+  color:white;
+ 
+
+  @include flex(row, center, center);
+}
+
+
+ul {
+    @include flex(row, center, center);
+    list-style-type: none;
+    gap: 1rem;
+    margin-top: 0px;
+}
+
+    a {
+      padding: 8px 10px;
+      text-decoration: none;
+     
+      dispaly:flex;
+    }
+
+
+
+</style>
